2512822ec5e636e481e436ae5de903bbdc25b7c1
yasinTawasulli <royesh14@gmail.com>
2023-02-01 07:49:35 -0800

    recilience4j

--- gateway-service/src/main/java/com/cydeo/GatewayServerApplication.java
+++ gateway-service/src/main/java/com/cydeo/GatewayServerApplication.java
@@ -10,8 +10,27 @@ public class GatewayServerApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(GatewayServerApplication.class, args);
-
+        SpringApplication.run(GatewayServerApplication.class,args);
     }
 
+    //    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(p -> p
+//                        .path("/cydeo/user/**")
+//                        .filters(f -> f.rewritePath("/cydeo/user/(?<segment>.*)","/${segment}")
+//                                .addResponseHeader("X-Response-Time",new Date().toString()))
+//                        .uri("lb://user-service"))
+//                .route(p -> p
+//                        .path("/cydeo/project/**")
+//                        .filters(f -> f.rewritePath("/cydeo/project/(?<segment>.*)","/${segment}")
+//                                .addResponseHeader("X-Response-Time",new Date().toString()))
+//                        .uri("lb://project-service"))
+//                .route(p -> p
+//                        .path("/cydeo/task/**")
+//                        .filters(f -> f.rewritePath("/cydeo/task/(?<segment>.*)","/${segment}")
+//                                .addResponseHeader("X-Response-Time",new Date().toString()))
+//                        .uri("lb://task-service"))
+//                .build();
+//    }
 }
